fa559926dc4cb97fcd3154133101552c8a31a9d2
Anyssa Buchanan <anyssab@bu.edu>
2014-05-30 01:59:55 -0400

    Initial Commit

---
+++ Crystal Ball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Crystal Ball
+//
+//  Created by Anyssa Buchanan on 5/30/14.
+//  Copyright (c) 2014 Anyssa Buchanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Crystal Ball/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Crystal Ball
+//
+//  Created by Anyssa Buchanan on 5/30/14.
+//  Copyright (c) 2014 Anyssa Buchanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
